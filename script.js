e701721dea410e96504d5bea4fbf8f8fac9fe816
NaveenkumarKTelkar <naveenkumartelkar26@gmail.com>
2023-11-30 22:07:05 +0530

    black theeme

--- script.js
+++ script.js
@@ -15,3 +15,35 @@ function dialogOpener() {
         icon.classList.add('fa-plus')
     }
 }
+
+
+// 
+var isLightTheme = true;
+
+const themeChanger = () =>{
+    isLightTheme = !isLightTheme;
+    const nav =document.getElementById('nav')
+    const topbar =document.getElementById('topbar')
+    const themeIcon = document.getElementById('theme')
+    if(isLightTheme){
+        themeIcon.classList.remove("fa-cloud-moon")
+        themeIcon.classList.add("fa-cloud-sun")
+
+        document.body.style.background= "#fff"
+        document.body.style.color= "#000"
+        // nav
+        nav.style.backgroundColor="#fff"
+        //topbar
+        topbar.style.backgroundColor="#fff"
+    }else{
+        themeIcon.classList.add("fa-cloud-moon")
+        themeIcon.classList.remove("fa-cloud-sun")
+
+        document.body.style.background= "#000"
+        document.body.style.color= "#fff"
+        // nav
+        nav.style.backgroundColor="#252525"
+        //topbar
+        topbar.style.backgroundColor="#252525"
+    }
+}
